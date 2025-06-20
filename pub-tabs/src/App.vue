e0786886bdc38317af859479520721e464964a19
mishdif <yoavishita@gmail.com>
2025-06-21 01:43:42 +0300

    Add 'TopHeader' component

--- pub-tabs/src/App.vue
+++ pub-tabs/src/App.vue
@@ -1,41 +1,37 @@
 <template>
-  <div>
-    <img class="icon" alt="Beer logo" src="./assets/beers-icon.png">
-    <h1>Welcom to PUB-TABS!</h1>
-  </div>
-  <AddTab></AddTab>
-   <TabsList></TabsList>
+  <TopHeader />
+  <!-- <AddTab></AddTab>
+   <TabsList></TabsList> -->
 </template>
 
 <script>
-import TabsList from './components/TabsList.vue';
-import AddTab from './components/AddTab.vue';
+import TopHeader from './components/TopHeader.vue';
+// import TabsList from './components/TabsList.vue';
+// import AddTab from './components/AddTab.vue';
 export default {
   name: 'App',
   components: {
-    TabsList,
-    AddTab
+    // TabsList,
+    // AddTab,
+    TopHeader
   }
 }
 </script>
 
 <style>
 body {
+  padding: 0;
+  margin: 0;
   background-image: url("@/assets/background-planks-texture.png");
   background: cover;
   background-size: cover;
-  background-repeat: no-repeat;
+  /* background-repeat: no-repeat; */
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-.icon {
-  width: 100px;
-  height: 100px;
+  color: white;
 }
 </style>
