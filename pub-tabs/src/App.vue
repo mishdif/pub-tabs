e3b0521cbe91f78660e483e9c286370b0b241f0f
mishdif <yoavishita@gmail.com>
2025-06-21 11:15:17 +0300

    Fix position for background and top-header

--- pub-tabs/src/App.vue
+++ pub-tabs/src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <TopHeader />
-  <!-- <AddTab></AddTab>
-   <TabsList></TabsList> -->
+  <header>
+    <TopHeader class="top-header"/>    
+  </header>
+  <main>
+    <TabsList class="tabs-list"/>
+  </main>
+  <!-- <AddTab></AddTab> -->
 </template>
 
 <script>
 import TopHeader from './components/TopHeader.vue';
-// import TabsList from './components/TabsList.vue';
+import TabsList from './components/TabsList.vue';
 // import AddTab from './components/AddTab.vue';
 export default {
   name: 'App',
   components: {
-    // TabsList,
+    TabsList,
     // AddTab,
     TopHeader
   }
@@ -19,13 +23,32 @@ export default {
 </script>
 
 <style>
-body {
-  padding: 0;
-  margin: 0;
+:root {
+  --header-height: 160px;
+}
+header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: var(--header-height);
+  z-index: 1000; /* to keep it above other elements */
+}
+main {
+  padding-top: var(--header-height);
+}
+body::before {
+  content: "";
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
   background-image: url("@/assets/background-planks-texture.png");
-  background: cover;
   background-size: cover;
-  /* background-repeat: no-repeat; */
+  background-repeat: no-repeat;
+  background-position: center;
+  z-index: -1; /* Push it behind everything */
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -34,4 +57,8 @@ body {
   text-align: center;
   color: white;
 }
+/* .tabs-list {
+  margin-top: 100px;
+  margin-bottom: 100px;
+} */
 </style>
